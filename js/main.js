6f6ac98238adf04ccf610ce833f72104f26c67d7
Yelysei <elisei2015@gmail.com>
2019-05-10 01:23:36 +0300

    sponsorship article, slider, photo animation rework

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+// jQuery TIme
+
+
+
 function getTimeRemaining(endtime) {
    var t = Date.parse(endtime) - Date.parse(new Date());
    var seconds = Math.floor((t / 1000) % 60);
@@ -39,3 +43,44 @@ function getTimeRemaining(endtime) {
   
  var deadline = new Date(Date.parse(new Date()) + 24 * 37 * 61 * 60 * 1000); // for endless timer
  initializeClock('clockdiv', deadline);
+ 
+
+
+//  CARDS ANIMATION
+
+const cards = findAllCards();
+bindEventsCard(cards);
+
+
+function findAllCards() {
+  return document.querySelectorAll('.card');
+}
+
+function bindEventsCard(cards) {
+  for (let i = 0, length = cards.length; i < length; i++) {
+    cards[i].addEventListener('mousemove', startRotate);
+    cards[i].addEventListener('mouseout', stopRotate);
+  }
+}
+
+function startRotate(event) {
+  let cardItem = this.querySelector('.card-item'),
+      halfHeight = cardItem.offsetHeight / 2,
+      halfWidth = cardItem.offsetWidth / 2;
+  
+  cardItem.style.transform = 'rotatex(' + -(event.offsetY - halfHeight) / 5 + 'deg) rotateY(' + (event.offsetX - halfWidth) / 5 + 'deg)';
+}
+
+function stopRotate(event) {
+  let cardItem = this.querySelector('.card-item');
+  cardItem.style.transform = 'rotate(0)';
+}
+jQuery(function($) {
+  $('.ba-sponsorship__list').slick({
+    infinite:true,
+    slidesToShow: 4,
+    slidesToScroll: 3
+  });
+  
+})
+
